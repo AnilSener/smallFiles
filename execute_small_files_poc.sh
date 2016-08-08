27f5bfa9f96a4e287ff68aca0d80e86ba18f6040
AnilSener <anil_sener@yahoo.com>
2016-08-08 09:05:16 -0700

    Default replication factor functionality is added

--- execute_small_files_poc.sh
+++ execute_small_files_poc.sh
@@ -11,6 +11,10 @@ USER=$(./properties_parser.sh HADOOP_USER $PROPERTIES_FILE)
 #USER=dt1dusr
 INPUT=/user/${USER}/input/Week_W201604/Farmatic/V/2
 OUTPUT=/user/${USER}/output
+REP=3
+if [ -x "$1" ];
+then REP=$1
+fi
 
 ${SMALL_POC_HOME}/execute_data_collection.sh
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
@@ -18,4 +22,5 @@ echo "well done!"
 echo "well done!" >>  ${SMALL_POC_HOME}/LAUNCHING_SPARK_SUBMIT.FLAG
 echo "input:  " ${INPUT}
 echo "output: " ${OUTPUT}
-spark-submit --master yarn-client  --conf "spark.authenticate.secret=true" ${SMALL_POC_HOME}/target/scala-2.10/smallFiles_Data_Acquisition.jar $1 $INPUT $OUTPUT
+
+spark-submit --master yarn-client  --conf "spark.authenticate.secret=true" ${SMALL_POC_HOME}/target/scala-2.10/smallFiles_Data_Acquisition.jar $REP $INPUT $OUTPUT
